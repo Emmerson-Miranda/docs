4232dc3a8af8853fb504a43e151e779b133ab6b0
Emmerson <emmerson.miranda@gmail.com>
2025-11-19 23:49:18 +0000

    Update help_menu.sh

--- bash/help_menu.sh
+++ bash/help_menu.sh
@@ -12,40 +12,47 @@ OPTIONS
   -h     display this help menu
   -r     do NOT press this button
   -a     echo something
+  -p    print passed parameters
 
 EXAMPLES
   ./help_menu.sh -a emmerson
   ./help_menu.sh -h
+  ./help_menu.sh -p arg1 arg2 arg3
   DEBUG=true ./help_menu.sh -a emmerson
 
 EOF
 }
 
-# Print all passed arguments
-echo "Passed arguments:"
-for arg in "$@"; do
-  echo "    argument: ${arg}"
-done
-echo ""
-
 # Check if no options were passed
 if [ "$#" -eq 0 ]; then echo "No options were passed"; _help; exit 1;fi
 
 # Parse command line options
-while getopts "ra:h" opt; do
+while getopts "r:a:hp" opt; do
   case "$opt" in
   r)
     curl 'https://ascii.live/rick'
     ;;
   a)
-    echo "PARAMETER -$opt: $OPTARG" # For example, "ECHO -a: foo"
+    echo "PARAMETER -$opt value $OPTARG" 
     ;;
   h)
     _help
     exit 0
     ;;
+  p)
+    # Print all passed arguments
+    echo "Passed arguments:"
+    for arg in "$@"; do
+      echo "    argument: ${arg}"
+    done
+    echo ""
+    ;;
+  :)
+    echo "Option -$opt requires an argument."
+    exit 0
+    ;;
   *)
-    _help
+    # print illegal option
     exit 0
     ;;
   esac
